8802fb052fff69511ff4ed818d0998a55b0b40fd
Haji Suleman <mirhajisulemanjamali@gmail.com>
2026-02-06 22:47:46 +0500

    Because of some complecation removing Getting model ready

--- model.py
+++ model.py
@@ -1,14 +1,28 @@
 import torch
-from getting_Model_Ready import getting_Model_Ready
 from cleaning import clean_Climate_Change_Dataset
 from sklearn.model_selection import train_test_split
+from torch import nn
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 df = clean_Climate_Change_Dataset()
+RANDOMSEED = 42
+X = df.drop(columns=["id", "climate_risk_index"]).values
+y = df["climate_risk_index"].values
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
 
-X, y = getting_Model_Ready(df=df)
+# Convert into tensor
+X = torch.tensor(X, dtype=torch.float32)
+y = torch.tensor(y, dtype=torch.float32).unsqueeze(1)
 
 
-def model(X: torch.tensor = X, y: torch.tensor = y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+print(len(X))
 
 
+class Climate_Change(nn.Module):
+    def __init__(self):
+        super().__init__()
